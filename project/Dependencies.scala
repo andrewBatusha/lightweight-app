abd39a4dffc35a97d86dfeb1017ac6bf31b70676
andrewBatusha <andrew.batuiev@gmail.com>
2020-12-30 21:27:39 +0200

    add cors

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalaLastVersion      = "2.12.4"
+  val scalaLastVersion = "2.12.4"
 
   val akkaVersion           = "2.4.20"
   val akkaHttpVersion       = "10.0.11"
@@ -18,10 +18,10 @@ object Dependencies {
   val bCryptScalaVersion    = "3.1"
   val jwtScalaVersion       = "0.14.1"
 
-  val scalaTestVersion      = "3.0.5"
-  val scalacheckVersion     = "1.13.5"
-  val scalaMeterVersion     = "0.9"
-  val scalaMockVersion      = "3.6.0"
+  val scalaTestVersion  = "3.0.5"
+  val scalacheckVersion = "1.13.5"
+  val scalaMeterVersion = "0.9"
+  val scalaMockVersion  = "3.6.0"
 
   val core = Seq(
     "com.typesafe.akka"        %% "akka-actor"           % akkaVersion,
@@ -29,31 +29,32 @@ object Dependencies {
     "com.typesafe.akka"        %% "akka-http"            % akkaHttpVersion,
     "com.typesafe.akka"        %% "akka-http-spray-json" % akkaHttpVersion,
     "com.softwaremill.macwire" %% "macros"               % macWireVersion % "provided",
-    "com.typesafe"             %  "config"               % typesafeConfigVersion,
+    "com.typesafe"             % "config"                % typesafeConfigVersion,
     "com.github.pureconfig"    %% "pureconfig"           % pureConfigVersion,
     "org.typelevel"            %% "cats-core"            % catsVersion,
-    "com.zaxxer"               %  "HikariCP"             % hikariCPVersion,
-    "org.postgresql"           %  "postgresql"           % postgresDriverVersion,
-    "org.flywaydb"             %  "flyway-core"          % flyWayVersion,
+    "com.zaxxer"               % "HikariCP"              % hikariCPVersion,
+    "org.postgresql"           % "postgresql"            % postgresDriverVersion,
+    "org.flywaydb"             % "flyway-core"           % flyWayVersion,
     "com.typesafe.slick"       %% "slick"                % slickVersion,
     "com.typesafe.slick"       %% "slick-hikaricp"       % slickVersion,
     "com.github.tminglei"      %% "slick-pg"             % slickPgVersion,
     "com.github.tminglei"      %% "slick-pg_spray-json"  % slickPgVersion,
     "com.github.tminglei"      %% "slick-pg_jts"         % slickPgVersion,
     "com.github.t3hnar"        %% "scala-bcrypt"         % bCryptScalaVersion,
-    "com.pauldijou"            %% "jwt-core"             % jwtScalaVersion
+    "com.pauldijou"            %% "jwt-core"             % jwtScalaVersion,
+    "ch.megard"                %% "akka-http-cors"       % "1.1.1"
   )
 
   val test = Seq(
-    "org.scalatest"       %% "scalatest"                   % scalaTestVersion,
-    "org.scalacheck"      %% "scalacheck"                  % scalacheckVersion,
-    "com.storm-enroute"   %% "scalameter"                  % scalaMeterVersion,
-    "org.scalactic"       %% "scalactic"                   % scalaTestVersion,
-    "org.scalamock"       %% "scalamock-scalatest-support" % scalaMockVersion,
-    "com.typesafe.akka"   %% "akka-testkit"                % akkaVersion,
-    "com.typesafe.akka"   %% "akka-stream-testkit"         % akkaVersion,
-    "com.typesafe.akka"   %% "akka-http-testkit"           % akkaHttpVersion,
-    "com.typesafe.slick"  %% "slick-testkit"               % slickVersion
+    "org.scalatest"      %% "scalatest"                   % scalaTestVersion,
+    "org.scalacheck"     %% "scalacheck"                  % scalacheckVersion,
+    "com.storm-enroute"  %% "scalameter"                  % scalaMeterVersion,
+    "org.scalactic"      %% "scalactic"                   % scalaTestVersion,
+    "org.scalamock"      %% "scalamock-scalatest-support" % scalaMockVersion,
+    "com.typesafe.akka"  %% "akka-testkit"                % akkaVersion,
+    "com.typesafe.akka"  %% "akka-stream-testkit"         % akkaVersion,
+    "com.typesafe.akka"  %% "akka-http-testkit"           % akkaHttpVersion,
+    "com.typesafe.slick" %% "slick-testkit"               % slickVersion
   ).map(_ % Test)
 
 }
--- src/main/scala/example/api/routers/ApiRouter.scala
+++ src/main/scala/example/api/routers/ApiRouter.scala
@@ -1,19 +1,21 @@
 package example.api.routers
 
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import example.api.directives.CORSHandler
 import example.api.protocol.{ApiJsonProtocol, ApiResponse}
 import example.services.{AccountService, AuthService, BeverageService, ReviewService}
 
 class ApiRouter(
-    accountService:    AccountService,
-    authService:       AuthService,
-    beverageService:   BeverageService,
-    reviewService:     ReviewService,
-) {
+    accountService:  AccountService,
+    authService:     AuthService,
+    beverageService: BeverageService,
+    reviewService:   ReviewService
+) extends CORSHandler {
 
-  val accountApiRouter    = new AccountApiRouter(accountService)
-  val authApiRouter       = new AuthApiRouter(authService)
-  val beverageApiRouter   = new BeverageApiRouter(beverageService)
-  val reviewApiRoute      = new ReviewApiRoute(reviewService)
+  val accountApiRouter  = new AccountApiRouter(accountService)
+  val authApiRouter     = new AuthApiRouter(authService)
+  val beverageApiRouter = new BeverageApiRouter(beverageService)
+  val reviewApiRoute    = new ReviewApiRoute(reviewService)
 
   import ApiJsonProtocol._
 
@@ -21,11 +23,13 @@ class ApiRouter(
   val routes: Route = handleRejections(ValidationRejectionHandler) {
     sessionFromAuthorizationHeader { implicit optionalSession =>
       pathPrefix("api" / "v1") {
+        corsHandler(
         healthRouter ~
           accountApiRouter.route ~
           authApiRouter.route  ~
           beverageApiRouter.route ~
           reviewApiRoute.route
+        )
       }
     }
   }
